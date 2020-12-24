1ce9336b06fc47be0bf3043c98c5b5ebecec2855
BackRunner <dev@backrunner.top>
2020-12-25 01:08:06 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+</script>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue';
+import { ElButton, ElInput, ElMessage } from 'element-plus';
+
+import App from './App.vue';
+import './styles/element-variable.scss';
+import './styles/main.scss';
+
+const app = createApp(App).mount('#app');
+
+// element plus
+const ElComponents = [ElButton, ElInput];
+const ElPlugins = [ElMessage];
+ElComponents.forEach((component) => {
+  app.component(component.name, component);
+});
+ElPlugins.forEach((plugin) => {
+  app.use(plugin);
+});
